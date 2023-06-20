cc25625429d81e38c717b43db7b211492f280fd4
utente <ghetto23@live.it>
2023-06-20 13:40:55 +0200

    ho aggiunto esempi di blocchi if and else

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,41 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Invio with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome to corso java!");
 
-        // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //if/else BASE
+        int numero = 10;
 
-            // Press Maiusc+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        if (numero > 0) {
+            System.out.println("Il numero è positivo.");
+        } else {
+            System.out.println("Il numero è negativo o zero.");
+        }
+
+        //if/if-else/else AVANZATO
+        int punteggio = 85;
+
+        if (punteggio >= 90) {
+            System.out.println("Hai ottenuto un voto eccellente!");
+        } else if (punteggio >= 80) {
+            System.out.println("Hai ottenuto un buon voto.");
+        } else if (punteggio >= 70) {
+            System.out.println("Hai ottenuto un voto sufficiente.");
+        } else {
+            System.out.println("Hai ottenuto un voto insufficiente.");
+        }
+
+        //if/else nested (annidato)
+        int eta = 25;
+        boolean haPatente = true;
+
+        if (eta >= 18) {
+            if (haPatente) {
+                System.out.println("Puoi guidare!");
+            } else {
+                System.out.println("Devi avere la patente per poter guidare.");
+            }
+        } else {
+            System.out.println("Sei troppo giovane per guidare.");
         }
     }
 }
